8cde245835cb550364a1b1013090bde06860c804
Hannah <hannahjo@Hannahs-MacBook-Pro.local>
2018-12-21 07:49:57 -0600

    inital

---
+++ main.js
@@ -0,0 +1,15 @@
+var mApp = new Vue ({
+  el: '#vue',
+  data: {
+    title: 'New Vue',
+    term:'',
+    songs: []
+  },
+  methods: {
+    search(){
+      fetch()
+      .then()
+      });
+    }
+  }
+})
